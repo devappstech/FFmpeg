65737c5eea8df6852d46b92b8e86ccd295646d19
gdLin600 <lingd600@163.com>
2016-07-03 00:33:05 +0800

    初始化

---
+++ FFmpegDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFmpegDemo
+//
+//  Created by 林广德 on 16/7/3.
+//  Copyright © 2016年 gdLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFmpegDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FFmpegDemo
+//
+//  Created by 林广德 on 16/7/3.
+//  Copyright © 2016年 gdLin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
